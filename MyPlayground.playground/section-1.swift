901f738c0c9235524ea1d2d84a7f4d016430cbdc
alxcancado <alxcancado@gmail.com>
2015-02-16 19:44:12 +1300

    first commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var name:String = "Alex"
+name = "Lima"
+
+var unknow:String
+unknow = "3"
+
+
+var a = 5
+var b = 3
+
+a * b
